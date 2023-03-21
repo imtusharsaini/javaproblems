3284611b9c5077e61883075467a21f6840806353
imtusharsaini <128510425+imtusharsaini@users.noreply.github.com>
2023-03-21 22:12:15 +0530

    Add files via upload

---
+++ Binary_s.java
@@ -0,0 +1,25 @@
+public class reverse {
+    public static int binarysearch(int number[] ){
+        int start =0;
+        int end = number.length;
+        while(start < end){
+
+           int temp = end;
+           end = first;
+           first = temp;
+           first++;
+           end--;
+        }
+
+    }
+    
+
+    public static void main (String[] argument){
+    
+        int number[] = {2,4,6,8,10};
+            
+        System.out.println("the array of index is :" + reverses(number)); 
+         
+    }
+    
+}
